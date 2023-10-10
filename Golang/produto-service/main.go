67d2c0db3c9b2ff11cd7c1d56dfeb44e2f508c32
Marcio Alexandre <marcio.alexandre83@gmail.com>
2023-10-10 17:04:00 +0100

    video Go #03: Primeira Api RestT: get e delete (enviando ID). Link: https://www.youtube.com/watch?v=sXYCvKsJG50&list=PL9K59CMzoAG-7ed0giNpXwy677yJBPwX6

--- Golang/produto-service/main.go
+++ Golang/produto-service/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -25,10 +27,55 @@ func addProduto(context *gin.Context) {
 	context.IndentedJSON(http.StatusCreated, "O produto foi registrado com sucesso!")
 }
 
+func parseToInt(id string) int {
+	id64, err := strconv.ParseInt(id, 10, 8)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return int(id64)
+}
+
+func getIndexById(id int) int {
+	index := -1
+	for i, produto := range produtos {
+		if produto.ID == id {
+			index = i
+		}
+	}
+	return index
+}
+
+func getProduto(context *gin.Context) {
+	id := context.Param("id") // formato string
+	index := getIndexById(parseToInt(id))
+
+	if index > -1 {
+		context.IndentedJSON(http.StatusFound, produtos[index])
+		return
+	}
+
+	context.IndentedJSON(http.StatusNotFound, "Produto inexistente!")
+}
+
+func deleteProduto(context *gin.Context) {
+	id := context.Param("id")
+	index := getIndexById(parseToInt(id))
+
+	if index > -1 {
+		produtos = append(produtos[:index], produtos[index+1:]...)
+		context.IndentedJSON(http.StatusOK, "Produto excluído com sucesso!")
+		return
+	}
+
+	context.IndentedJSON(http.StatusNotFound, "Produto inexistente para exclusão!")
+}
+
 func main() {
 	router := gin.Default()
 	router.POST("/produto", addProduto)
-	router.GET("/produtos", getProdutos)
+	router.GET("/produto", getProdutos)
 	router.GET("/hello", hello)
+	router.GET("/produto/:id", getProduto)
+	router.DELETE("/produto/:id", deleteProduto)
 	router.Run("localhost:8080")
 }
